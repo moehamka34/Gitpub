a1071df276925be53609d8cea2e3f2682e354eaf
Mohamed Hamka <mohamedhamka@Mohameds-MBP-2.attlocal.net>
2022-05-17 16:58:42 -0400

    database is done

--- server.js
+++ server.js
@@ -1,51 +1,12 @@
 const express = require("express");
+const drinks = require('./models/drinks');
 const app = express();
+
 
 const port = 3000;
 
 
-const drinks = [
-    {
-      name: "cruddy mary",
-      price: 132,
-      image: "https://i.imgur.com/Va5iIw5",
-    },
-    {
-      name: "index on the beach",
-      price: 68,
-      image: "https://i.imgur.com/XV2aPa2",
-    },
-    {
-      name: "hack & coke",
-      price: 1,
-      image: "https://i.imgur.com/rLOXFRI",
-    },
-    {
-      name: "whiskey-value pair",
-      price: 11,
-      image: "https://i.imgur.com/a7rmkoS",
-    },
-    {
-      name: "404 horsemen",
-      price: 202,
-      image: "https://i.imgur.com/FLucOLr",
-    },
-    {
-      name: "if stateMint julep",
-      price: 2,
-      image: "https://i.imgur.com/yaoK0Dg",
-    },
-    {
-      name: "APIPA",
-      price: 43,
-      image: "https://i.imgur.com/qAhA7pi",
-    },
-    {
-      name: "node to joy",
-      price: 56,
-      image: "https://i.imgur.com/MbVdwZz",
-    },
-  ]
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
@@ -54,6 +15,7 @@ app.get('/', (req, res) => {
 app.get('/drinks', (req, res) => {
     res.send(drinks)
 });
+
 
 app.listen(port, () => {
     console.log("listening..");
